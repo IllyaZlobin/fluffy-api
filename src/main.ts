025aa1f508b4cb31dd0c3bc2def88dccff001344
DESKTOP-26CSUBO\illia <illiazlobin@gmail.com>
2020-12-21 23:50:06 +0200

    main bootstrap

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,20 @@
+import { ConfigService } from '@nestjs/config';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { setupSwagger } from './setups';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+
+  const configService = await app.get(ConfigService);
+
+  setupSwagger(app, 'FlixGo API', 'List of apis', '1.0.0');
+
+  const port = await configService.get('port');
+  const env = await configService.get('env');
+
+  await app.listen(port);
+
+  console.log(`APP RUN ON ${port}, ENV - ${env}`);
 }
 bootstrap();
