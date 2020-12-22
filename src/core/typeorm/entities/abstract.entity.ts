66f5c77bb023cf205b68ee0fd432657514ae66af
DESKTOP-26CSUBO\illia <illiazlobin@gmail.com>
2020-12-22 23:41:03 +0200

    base entity model

---
+++ src/core/typeorm/entities/abstract.entity.ts
@@ -0,0 +1,17 @@
+import {
+  CreateDateColumn,
+  PrimaryGeneratedColumn,
+  UpdateDateColumn,
+} from 'typeorm';
+import { BaseEntity } from '../interfaces';
+
+export abstract class AbstractEntity implements BaseEntity {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @CreateDateColumn()
+  createdAt: Date;
+
+  @UpdateDateColumn()
+  updatedAt: Date;
+}
