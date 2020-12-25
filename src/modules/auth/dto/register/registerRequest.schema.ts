a03378be4226cc5d2448bd4d5ba683d0d7c9a0e2
DESKTOP-26CSUBO\illia <illiazlobin@gmail.com>
2020-12-25 22:08:38 +0200

    fix user registration schema

--- src/modules/auth/dto/register/registerRequest.schema.ts
+++ src/modules/auth/dto/register/registerRequest.schema.ts
@@ -1,28 +1,19 @@
 import * as Joi from '@hapi/joi';
-import { Gender, Roles } from 'src/core';
 import { RegisterRequest } from './register.request';
 
 export const RegisterRequestSchema = Joi.object<RegisterRequest>({
-  firstname: Joi.string().required(),
-  lastname: Joi.string().required(),
+  firstName: Joi.string().required(),
   email: Joi.string()
     .email()
     .required(),
   password: Joi.string()
     .min(6)
     .required(),
-  role: Joi.string()
-    .valid(...Object.keys(Roles))
-    .required(),
-  gender: Joi.string()
-    .valid(...Object.keys(Gender))
-    .required(),
   phone: Joi.string()
     .min(8)
     .required(),
-  zipCode: Joi.string().required(),
-  address: Joi.string().required(),
-  country: Joi.number()
+  countryId: Joi.number()
     .integer()
+    .positive()
     .min(1),
 });
