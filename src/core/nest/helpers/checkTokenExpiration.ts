919cb886e7380b047db68e8fbbf566d38532d2d2
DESKTOP-26CSUBO\illia <illiazlobin@gmail.com>
2021-01-04 18:36:52 +0200

    Fix tokens exp checker

--- src/core/nest/helpers/checkTokenExpiration.ts
+++ src/core/nest/helpers/checkTokenExpiration.ts
@@ -4,12 +4,11 @@
  * @description check expiration of token
  */
 export const isTokenTokenExpired = async (exp: number): Promise<boolean> => {
-  const now = new Date(Date.now()).toLocaleString();
-  const expiration = new Date(exp * 1000).toLocaleString();
-
-  if (expiration < now) {
-    return true;
-  } else {
+  const now = Math.floor(Date.now() / 1000)
+  
+  if (exp > now) {
     return false;
+  } else {
+    return true;
   }
 };
