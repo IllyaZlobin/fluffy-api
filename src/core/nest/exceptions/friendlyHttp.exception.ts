a03c2f7901e52e578ed223a31f1688f4a048ae22
DESKTOP-26CSUBO\illia <illiazlobin@gmail.com>
2021-01-31 19:55:29 +0200

    added status to FriendlyHttpException

--- src/core/nest/exceptions/friendlyHttp.exception.ts
+++ src/core/nest/exceptions/friendlyHttp.exception.ts
@@ -2,7 +2,7 @@ import { HttpException, HttpStatus } from '@nestjs/common';
 
 export class FriendlyHttpException extends HttpException {
   property: string[];
-  constructor(message, property?: string[]) {
+  constructor(status: HttpStatus, message, property?: string[]) {
     super(message, HttpStatus.OK);
     this.property = property || [];
   }
