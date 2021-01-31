bf5e31a32d8ae204e72563cdc4aa98a5d0913689
DESKTOP-26CSUBO\illia <illiazlobin@gmail.com>
2021-01-31 19:59:40 +0200

    auth flow for cognito

--- src/modules/auth/services/auth.service.ts
+++ src/modules/auth/services/auth.service.ts
@@ -1,20 +1,26 @@
-import { Injectable } from '@nestjs/common';
+import { HttpStatus, Injectable, UnauthorizedException } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import {
   CountryEntity,
   IsEntityExist,
+  Roles,
   UserEntity,
 } from '../../../core/typeorm';
 import { Repository } from 'typeorm';
 import { LoginRequest } from '../dto/login/login.request';
-import { SessionService } from './session.service';
-import {
-  FriendlyHttpException,
-  JwtPayload,
-  ValidationException,
-} from 'src/core';
+import { FriendlyHttpException, ValidationException } from 'src/core';
 import { RegisterRequest } from '../dto/register/register.request';
 import { UserService } from 'src/modules/user/services/user.service';
+import {
+  AuthenticationDetails,
+  CognitoAccessToken,
+  CognitoIdToken,
+  CognitoRefreshToken,
+  CognitoUser,
+  CognitoUserAttribute,
+  CognitoUserPool,
+  CognitoUserSession,
+} from 'amazon-cognito-identity-js';
 
 @Injectable()
 export class AuthService {
@@ -23,39 +29,47 @@ export class AuthService {
     private readonly userRepo: Repository<UserEntity>,
     @InjectRepository(CountryEntity)
     private readonly countryRepo: Repository<CountryEntity>,
-    private readonly sessionService: SessionService,
     private readonly userService: UserService,
+    private cognitoPool: CognitoUserPool,
   ) {}
 
-  async login(
-    model: LoginRequest,
-    remoteAddress: string,
-  ): Promise<{ accessToken: string; refreshToken?: string }> {
-    const user = await IsEntityExist<UserEntity>(this.userRepo, {
-      email: model.email,
+  async login(model: LoginRequest): Promise<CognitoUserSession> {
+    const { email, password } = model;
+
+    const authDetails = new AuthenticationDetails({
+      Username: email,
+      Password: password,
     });
 
-    if (user) {
-      await this.checkPassword(user, model.password);
-      const tokens = await this.handleUserSession(user, remoteAddress);
-      return tokens;
-    } else {
-      throw new FriendlyHttpException('User is not found', ['email']);
-    }
-  }
+    const user = new CognitoUser({ Pool: this.cognitoPool, Username: email });
 
-  async refreshToken(
-    payload: JwtPayload,
-  ): Promise<{ accessToken: string; refreshToken?: string }> {
-    const user = await IsEntityExist<UserEntity>(this.userRepo, {
-      id: payload.id,
+    const result = new Promise((res, rej) => {
+      user.authenticateUser(authDetails, {
+        onSuccess: result => {
+          res(result);
+        },
+        onFailure: err => {
+          rej(err);
+        },
+      });
     });
 
-    const tokens = await this.handleUserSession(user);
-    return tokens;
+    const {
+      getAccessToken,
+      getIdToken,
+      getRefreshToken,
+    } = (await result) as CognitoUserSession;
+
+    return new CognitoUserSession({
+      AccessToken: getAccessToken(),
+      IdToken: getIdToken(),
+      RefreshToken: getRefreshToken(),
+    });
   }
 
-  async register(model: RegisterRequest): Promise<UserEntity> {
+  async register(model: RegisterRequest) {
+    const { email, firstName, password, phone } = model;
+
     const country = await IsEntityExist<CountryEntity>(this.countryRepo, {
       id: model.countryId,
     });
@@ -69,51 +83,83 @@ export class AuthService {
     });
 
     if (existedUser) {
-      throw new FriendlyHttpException('User is already exist');
+      throw new FriendlyHttpException(HttpStatus.OK, 'User is already exist');
     } else {
-      const result = await this.userService.create({ ...model, country });
-      return result;
-    }
-  }
-
-  /**
-   *
-   * @param password - password from request
-   * @description compare user password to passed password
-   */
-  private async checkPassword(
-    user: UserEntity,
-    password: string,
-  ): Promise<boolean> {
-    const isValid = await user.validatePassword(password);
-
-    if (!isValid) {
-      throw new FriendlyHttpException('Wrong password', ['password']);
-    }
-    return isValid;
-  }
-
-  /**
-   *
-   * @param user
-   * @param remoteAddress - ip address
-   * @description Check if user have a session then update access token. If user don't have a session - create new session and return tokens
-   */
-  private async handleUserSession(
-    user: UserEntity,
-    remoteAddress?: string,
-  ): Promise<{ accessToken: string; refreshToken?: string }> {
-    const session = await this.sessionService.getByUser(user);
-
-    if (session) {
-      const tokens = await this.sessionService.update(session);
-      return tokens;
-    } else {
-      const { accessToken, refreshToken } = await this.sessionService.create(
-        user,
-        remoteAddress,
+      const attributeList = [];
+      attributeList.push(
+        new CognitoUserAttribute({ Name: 'name', Value: firstName }),
       );
-      return { accessToken, refreshToken };
+      attributeList.push(
+        new CognitoUserAttribute({ Name: 'phone_number', Value: phone }),
+      );
+      attributeList.push(
+        new CognitoUserAttribute({ Name: 'custom:role', Value: Roles.USER }),
+      );
+
+      const cognitoUserSub = new Promise((resolve, reject) => {
+        this.cognitoPool.signUp(
+          email,
+          password,
+          attributeList,
+          null,
+          (err, result) => {
+            if (err) {
+              reject(err);
+            } else {
+              resolve(result.userSub);
+            }
+          },
+        );
+      });
+
+      const sub = (await cognitoUserSub) as string;
+
+      const user = await this.userService.create({
+        email,
+        firstName,
+        phone,
+        country,
+        sub,
+      });
+
+      return user;
     }
+  }
+
+  async refreshSession(idToken: string, refreshToken: string) {
+    const IdToken = new CognitoIdToken({ IdToken: idToken });
+    const RefreshToken = new CognitoRefreshToken({
+      RefreshToken: refreshToken,
+    });
+
+    if (!IdToken.payload.email) {
+      throw new UnauthorizedException('IdToken is not valid');
+    }
+
+    const user = await IsEntityExist<UserEntity>(this.userRepo, {
+      email: IdToken.payload.email,
+    });
+
+    if (!user) {
+      throw new FriendlyHttpException(
+        HttpStatus.NOT_FOUND,
+        'User is not found',
+        ['email'],
+      );
+    }
+
+    const cognitoUser = new CognitoUser({
+      Pool: this.cognitoPool,
+      Username: IdToken.payload.email,
+    });
+
+    return new Promise<CognitoUserSession>((res, rej) => {
+      cognitoUser.refreshSession(RefreshToken, (err, session) => {
+        if (err) {
+          rej(err);
+        }
+        res(session);
+      });
+    });
   }
 }
