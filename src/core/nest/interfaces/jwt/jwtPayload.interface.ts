90873e4806990b6fc3b0646fe127181a51f1b1c0
DESKTOP-26CSUBO\illia <illiazlobin@gmail.com>
2021-01-31 19:57:11 +0200

    jwt payload

--- src/core/nest/interfaces/jwt/jwtPayload.interface.ts
+++ src/core/nest/interfaces/jwt/jwtPayload.interface.ts
@@ -1,10 +1,17 @@
 import { TokenType } from '../../enums';
 
 export interface JwtPayload {
-  id: number;
-  email?: string;
-  type: TokenType;
-  iat: number;
-  exp: number;
+  sub: string;
+  email_verifies: boolean;
   iss: string;
+  phone_number_verified: boolean;
+  'cognito:username': string;
+  aud: string;
+  event_id: string;
+  token_user: string;
+  auth_time: string;
+  phone_number: string;
+  exp: string;
+  iat: number;
+  email: string;
 }
