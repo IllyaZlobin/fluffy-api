46dbf5b3daccb9ec8c0c18c1ea6cf7adcfb3eb2e
Illia Zlobin <illiazlobin@gmail.com>
2021-04-04 23:00:15 +0300

    test deploy

--- src/modules/bootstrap/bootstrap.module.ts
+++ src/modules/bootstrap/bootstrap.module.ts
@@ -5,18 +5,12 @@ import { S3Service } from './services/s3.service';
 
 @Module({
   imports: [
-    //ConfigModule.forRoot({
-    //  envFilePath: !env
-    //    ? Path.resolve('./env/.local.env')
-    //    : Path.resolve(`./env/.${env}.env`),
-    //  isGlobal: true,
-    //}),
     TypeOrmModule.forRootAsync({
       useFactory: (configService: ConfigService) => {
         return configService.TypeOrmConfig;
       },
       inject: [ConfigService],
-      imports: [BootstrapModule]
+      imports: [BootstrapModule],
     }),
   ],
   providers: [ConfigService, S3Service],
